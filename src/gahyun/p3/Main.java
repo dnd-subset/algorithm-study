cbdbf3cc8c52d896c05d612efa26f3748a36601b
hs12 <loveanna1228@gmail.com>
2024-09-29 15:42:01 +0900

    refactor : 입력 시 안 익은 사과 개수 카운트 후 bfs에서 줄여나가기

--- src/gahyun/p3/Main.java
+++ src/gahyun/p3/Main.java
@@ -9,8 +9,8 @@ import java.util.StringTokenizer;
 
 /*
 	- 문제 링크: https://www.acmicpc.net/problem/7569
-	- 메모리: 124556KB
-	- 시간: 576ms
+	- 메모리: 125604KB
+	- 시간: 592ms
  */
 /*
 	- BFS 탐색 로직은 1번과 동일, 차원만 증가
@@ -27,7 +27,7 @@ public class Main {
 	static int[] dy = {-1, 1, 0, 0, 0, 0};
 	static int[] dz = {0, 0, 0, 0, 1, -1};
 
-	static int h, n, m;
+	static int h, n, m, remain;
 
 	static void BFS() {
 		while (!queue.isEmpty()) {
@@ -38,6 +38,7 @@ public class Main {
 				int nz = cp.z + dz[i];
 				if (nx >= 0 && nx < n && ny >= 0 && ny < m && nz >= 0 && nz < h && graph[nz][nx][ny] == 0) {
 					graph[nz][nx][ny] = 1;
+					remain--;
 					queue.offer(new Pos(nx, ny, nz));
 					dis[nz][nx][ny] = dis[cp.z][cp.x][cp.y] + 1;
 				}
@@ -60,6 +61,7 @@ public class Main {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < m; j++) {
 					graph[k][i][j] = Integer.parseInt(st.nextToken());
+					if (graph[k][i][j]==0) remain++;
 					if (graph[k][i][j] == 1) {
 						queue.offer(new Pos(i, j, k));
 					}
@@ -67,33 +69,22 @@ public class Main {
 			}
 		}
 		BFS();
-		boolean flag = true; // 사과 익었는지 여부
 
+		int ans = 0;
 		for (int k = 0; k < h; k++) {
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < m; j++) {
-					if (graph[k][i][j] == 0) {
-						flag = false;
-						break;
-					}
+					ans = Math.max(ans, dis[k][i][j]);
 				}
 			}
 		}
-		int ans = 0;
-		if (flag) {
-			for (int k = 0; k < h; k++) {
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < m; j++) {
-						ans = Math.max(ans, dis[k][i][j]);
-					}
-				}
-			}
-			System.out.println(ans);
-		} else {
+
+		if (remain >0) {
 			System.out.println(-1);
+		} else {
+			System.out.println(ans);
 		}
 	}
-
 }
 
 class Pos {

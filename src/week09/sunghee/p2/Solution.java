1d547896d0f6246eab18cc91378041d6ac105f16
hee9841 <lee98417716@gmail.com>
2025-07-04 17:36:54 +0900

    rename : p1, p2 바뀜

--- src/week09/sunghee/p2/Solution.java
+++ src/week09/sunghee/p2/Solution.java
@@ -1,27 +1,50 @@
 package week09.sunghee.p2;
 
+
 import java.util.*;
 
-
 public class Solution {
-    public int solution(int[] scoville, int K) {
-        //우선 순위 큐 사용
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+    private static final int num = 65536;
 
-        for (int s : scoville) {
-            queue.add(s);
+    public int solution(String str1, String str2) {
+
+        //1. A, B 생성 (대문자로 변경, 영문자만 가능)
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        List<Integer> A = toMultiSet(str1);
+        List<Integer> B = toMultiSet(str2);
+
+        if(A.isEmpty() && B.isEmpty()) return (num);
+
+
+        int intersectCount = 0;
+
+        for(Integer aValue : A) {
+            boolean hasValue = B.remove(aValue);
+            if (!hasValue) continue;
+            intersectCount++;
         }
-        int answer = 0;
 
-        while(queue.peek() < K) {
-            if(queue.size() == 1) {
-                return -1;
-            }
+        int unionCount = A.size() + B.size();
+        double result = ((double) intersectCount / unionCount) * num;
 
-            queue.add(queue.poll() + queue.poll() * 2);
-            answer++;
+
+        return (int) result;
+    }
+
+    private List<Integer> toMultiSet(String str) {
+        List<Integer> multiset = new ArrayList<>();
+        for (int i = 0; i < str.length() -1; i++) {
+            int cur = str.charAt(i);
+            int next = str.charAt(i+1);
+            if (isNotAlphabet(cur) || isNotAlphabet(next)) continue;
+            multiset.add(cur * 100 + next);
         }
+        return multiset;
+    }
 
-        return answer;
+    private boolean isNotAlphabet(int char1) {
+        return char1 < 65 || char1 > 90;
     }
 }
